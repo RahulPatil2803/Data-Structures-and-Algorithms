81b71e351604ebf507cd2e6c9726922b94a03255
RahulPatil2803 <160585889+RahulPatil2803@users.noreply.github.com>
2024-10-14 23:21:58 +0530

    Add files via upload

---
+++ 1_Patterns/Similar_no.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main(){
+
+	// i=rows=5
+	// j=Columns=5
+
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=5;j++)
+		{
+			printf("%d  ",j);
+		}
+    printf("\n");
+	}
+	return 0;
+}
